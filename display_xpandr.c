d0b6b0903ecd68db82e72e3ee061fec608e0f6a3
kounch <kounch@users.noreply.github.com>
2019-01-17 20:08:55 +0100

    Added logging functionality

--- display_xpandr.c
+++ display_xpandr.c
@@ -26,10 +26,6 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <stdio.h>
-#include <SDL.h>
-#include <SDL_image.h>
-
 #include "display_xpandr.h"
 
 int main(int argc, char *argv[])
@@ -42,6 +38,7 @@ int main(int argc, char *argv[])
 
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_JPG);
+    TTF_Init();
 
     SDL_Window *window;
     window = SDL_CreateWindow("", SDL_WINDOW_X, SDL_WINDOW_Y, SDL_WINDOW_W, SDL_WINDOW_H,
@@ -53,7 +50,7 @@ int main(int argc, char *argv[])
     }
     else
     {
-        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_FLAGS);
         if (renderer == NULL)
         {
             printf("Could not create renderer: %s\n", SDL_GetError());
@@ -61,21 +58,19 @@ int main(int argc, char *argv[])
         }
         else
         {
-#ifdef _WIN32
-            if (argv[1][1] == ':')
-#else
-            if (argv[1][0] == '/')
-#endif
+            if (check_path(argv[1]))
             {
                 SDL_Surface *image = IMG_Load(argv[1]);
                 SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
-                SDL_RenderCopy(renderer, texture, NULL, NULL);
-                SDL_RenderPresent(renderer);
 
                 SDL_Event event;
                 int run_display = 1;
                 while (run_display)
                 {
+                    SDL_RenderCopy(renderer, texture, NULL, NULL);
+                    show_logs(window);
+                    SDL_RenderPresent(renderer);
+
                     SDL_PollEvent(&event);
                     if (event.type == SDL_QUIT)
                     {
@@ -90,8 +85,149 @@ int main(int argc, char *argv[])
         }
         SDL_DestroyWindow(window);
     }
-
     IMG_Quit();
     SDL_Quit();
     return 0;
+}
+
+void show_logs(SDL_Window *window)
+{
+    SDL_Surface *image;
+    SDL_Rect box;
+
+    image = SDL_GetWindowSurface(window);
+    if (image == NULL)
+    {
+        printf("Could not get surface: %s\n", SDL_GetError());
+        return;
+    }
+    box.w = image->w / 5 * 3;
+    box.h = image->h / 2;
+    box.x = image->w / 5;
+    box.y = box.h / 4;
+    if (check_file(LOG_FNAME)) 
+    {
+        draw_txt(window, box);
+    }
+}
+
+void draw_txt(SDL_Window *window, SDL_Rect box)
+{
+    char **textlines;
+    char **errlines;
+    int i;
+
+    textlines = (char **)malloc(LINES * sizeof(char *));
+    //allocate space for each string
+    for (i = 0; i < LINES; i++)
+    {
+        textlines[i] = (char *)malloc(LINEMAX * sizeof(char));
+    }
+
+    //Initialize text drawing area
+    SDL_Rect text_box = box;
+    text_box.x += 10;
+    text_box.y += 10;
+
+    //Read log
+    read_txt(LOG_FNAME, textlines);
+    for (i = 0; i < LINES; i++)
+    {
+        draw_txtline(window, &text_box, textlines[i]);
+    }
+
+    for (i = 0; i < LINES; i++)
+    {
+        free(textlines[i]);
+    }
+    free(textlines);
+}
+
+void draw_txtline(SDL_Window *window, SDL_Rect *text_box, char *txtline)
+{
+    TTF_Font *font;
+    SDL_Renderer *renderer;
+    SDL_Surface *surfaceMessage;
+    SDL_Texture *Message;
+    SDL_Color textColor = {255, 255, 255};
+    
+    if (starts_with(txtline, "ERR"))
+    {
+        textColor.r = 255;
+        textColor.g = 0;
+        textColor.b = 0;
+    } 
+    else if(starts_with(txtline, "WARN"))
+    {
+        textColor.r = 255;
+        textColor.g = 255;
+        textColor.b = 0;
+    }    
+
+    font = TTF_OpenFont(FONT_PATH, 24);
+    if (font == NULL)
+    {
+        printf("Error opening font: %s\n", TTF_GetError());
+        return;
+    }
+    renderer = SDL_GetRenderer(window);
+    if (strlen(txtline))
+    {
+        TTF_SizeText(font, txtline, &text_box->w, &text_box->h);
+        surfaceMessage = TTF_RenderText_Blended(font, txtline, textColor);
+        Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+        SDL_RenderCopy(renderer, Message, NULL, text_box);
+        SDL_DestroyTexture(Message);
+        SDL_FreeSurface(surfaceMessage);
+    }
+    text_box->y += text_box->h;
+    TTF_CloseFont(font);
+}
+
+void read_txt(char *fname, char *txtlines[])
+{
+    FILE *f;
+    int cnt;
+    int t;
+    int i;
+    char buf[LINEMAX];
+
+    //Empty destination strings
+    for (i = 0; i < LINES; i++)
+    {
+        txtlines[i][0] = '\0';
+    }
+
+    if (check_file(fname))
+    {
+        //Count lines in file
+        cnt = 0;
+        f = fopen(fname, "r");
+        while (fgets(buf, LINEMAX, f))
+        {
+            cnt++;
+        }
+        //Get last lines of file
+        t = 0;
+        i = 0;
+        fseek(f, 0, SEEK_SET);
+        while (fgets(buf, LINEMAX, f))
+        {
+            buf[strcspn(buf, "\n\r")] = '\0'; //Remove end of line
+            t++;
+            if (t > cnt - LINES)
+                strncpy(txtlines[i++], buf, LINEMAX * sizeof(char));
+        }
+        fclose(f);
+    }
+}
+
+_Bool starts_with(const char *string, const char *prefix)
+{
+    while(*prefix)
+    {
+        if(*prefix++ != *string++)
+            return 0;
+    }
+    return 1;
 }
--- display_xpandr.h
+++ display_xpandr.h
@@ -26,25 +26,70 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <stdio.h>
 
+#ifdef _WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <string.h>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 
 #ifdef __APPLE__
-    #define SDL_WINDOW_X SDL_WINDOWPOS_CENTERED
-    #define SDL_WINDOW_Y SDL_WINDOWPOS_CENTERED
-    #define SDL_WINDOW_W 1280
-    #define SDL_WINDOW_H 720
-    #define SDL_WINDOW_FLAGS SDL_WINDOW_SHOWN
+#define SDL_WINDOW_X SDL_WINDOWPOS_CENTERED
+#define SDL_WINDOW_Y SDL_WINDOWPOS_CENTERED
+#define SDL_WINDOW_W 1280
+#define SDL_WINDOW_H 720
+#define SDL_WINDOW_FLAGS SDL_WINDOW_SHOWN
+#define SDL_RENDERER_FLAGS SDL_RENDERER_SOFTWARE
+
+#define LOG_FNAME "/tmp/xpandr.log"
+#define FONT_PATH "/Users/kounch/Developer/PlayStationClassic/psxc_xpandr/SST-Medium.ttf"
+
 #elif _WIN32
-    #define SDL_WINDOW_X SDL_WINDOWPOS_CENTERED
-    #define SDL_WINDOW_Y SDL_WINDOWPOS_CENTERED
-    #define SDL_WINDOW_W 1280
-    #define SDL_WINDOW_H 720
-    #define SDL_WINDOW_FLAGS SDL_WINDOW_SHOWN
+#define SDL_WINDOW_X SDL_WINDOWPOS_CENTERED
+#define SDL_WINDOW_Y SDL_WINDOWPOS_CENTERED
+#define SDL_WINDOW_W 1280
+#define SDL_WINDOW_H 720
+#define SDL_WINDOW_FLAGS SDL_WINDOW_SHOWN
+#define SDL_RENDERER_FLAGS 0
+
+#define LOG_FNAME "D:\\dn10434\\Downloads\\display\\xpandr.log"
+#define FONT_PATH "D:\\dn10434\\Downloads\\display\\SST-Medium.ttf"
+
 #else
-    #define SDL_WINDOW_X SDL_WINDOWPOS_UNDEFINED
-    #define SDL_WINDOW_Y SDL_WINDOWPOS_UNDEFINED
-    #define SDL_WINDOW_W 1280
-    #define SDL_WINDOW_H 720
-    #define SDL_WINDOW_FLAGS SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS
+#define SDL_WINDOW_X SDL_WINDOWPOS_UNDEFINED
+#define SDL_WINDOW_Y SDL_WINDOWPOS_UNDEFINED
+#define SDL_WINDOW_W 1280
+#define SDL_WINDOW_H 720
+#define SDL_WINDOW_FLAGS SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS
+#define SDL_RENDERER_FLAGS 0
+
+#define LOG_FNAME "/tmp/xpandr.log"
+#define FONT_PATH "/usr/sony/share/data/font/SST-Medium.ttf"
 #endif
+
+#ifdef _WIN32
+#define check_path(path) (path[1] == ':')
+#else
+#define check_path(path) (path[0] == '/')
+#endif
+
+#ifdef _WIN32
+#define check_file(filepath) (_access(filepath, 0) == 0)
+#else
+#define check_file(filepath) (access(filepath, R_OK) == 0)
+#endif
+
+#define LINEMAX 100
+#define LINES 10
+
+void show_logs(SDL_Window *window);
+void draw_txt(SDL_Window *window, SDL_Rect box);
+void draw_txtline(SDL_Window *window, SDL_Rect *text_box, char *txtline);
+void read_txt(char *fname, char *textlines[]);
+_Bool starts_with(const char *string, const char *prefix);
